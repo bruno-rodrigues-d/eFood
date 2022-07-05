4c386bf200775aedc7094f348059edcf9603ea05
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2022-07-05 08:01:54 -0300

    Adicionando css e footer

--- e-food/src/components/Header/Header.jsx
+++ e-food/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 
 
-import React from 'react';
+import React, {useRef} from 'react';
 import { Container } from 'reactstrap';
 import logo from '../../assets/images/res-logo.png';
 import { NavLink, Link } from "react-router-dom";
+
+import '../../styles/header.css';
 
 const nav__links = [
   {
@@ -25,6 +27,9 @@ const nav__links = [
 ];
 
 const Header = () => {
+
+  const menuRef = useRef(null);
+  const toggleMenu = () => menuRef.current.classList.toggle('show__menu');
   return (
     <header className="header">
       <Container>
@@ -35,8 +40,8 @@ const Header = () => {
           </div>
 
           {/* ======= menu ======= */}
-          <div className="navigation" >
-            <div className="menu">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu} >
+            <div className="menu d-flex align-items-center gap-5">
               {nav__links.map((item, index) => (
                 <NavLink
                   to={item.path}
@@ -51,7 +56,7 @@ const Header = () => {
           <div className="nav__right d-flex align-items-center gap-4">
             <span className="cart__icon">
               <i class="ri-shopping-basket-line"></i>
-              <span className="cart__badge"></span>
+              <span className="cart__badge">2</span>
             </span>
 
             <span className="user">
@@ -60,7 +65,7 @@ const Header = () => {
               </Link>
             </span>
 
-            <span className="mobile__menu">
+            <span className="mobile__menu" onClick={toggleMenu}>
               <i class="ri-menu-line"></i>
             </span>
           </div>
